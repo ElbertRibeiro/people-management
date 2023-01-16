a2e06267af5f35f727985fd523667df5d23e7229
elbertribeiro <elbertprofissional@gmail.com>
2023-01-16 00:16:05 -0300

    refactory: migration java for kotlin

---
+++ src/main/kotlin/one/digitalinnovation/personapi/person/PersonController.kt
@@ -0,0 +1,45 @@
+package one.digitalinnovation.personapi.person
+
+import one.digitalinnovation.personapi.dto.MessageResponseDTO
+import one.digitalinnovation.personapi.exception.PersonNotFoundException
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+import javax.validation.Valid
+
+@RestController
+@RequestMapping("/api/v2/people")
+class PersonController(val personService: PersonService) {
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    fun listAll(): List<PersonDTO> {
+        return personService.listAll()
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createPerson(@RequestBody personDTO: @Valid PersonDTO): MessageResponseDTO {
+        return personService.createPerson(personDTO)
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @Throws(PersonNotFoundException::class)
+    fun findById(@PathVariable id: Long): PersonDTO {
+        return personService.findById(id)
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    @Throws(PersonNotFoundException::class)
+    fun updateById(@PathVariable id: Long, @RequestBody personDTO: @Valid PersonDTO): MessageResponseDTO {
+        return personService.updateById(id, personDTO)
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @Throws(PersonNotFoundException::class)
+    fun deleteById(@PathVariable id: Long) {
+        personService.delete(id)
+    }
+}
