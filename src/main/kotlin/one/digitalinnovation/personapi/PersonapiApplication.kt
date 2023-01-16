732c9f25c0ee8506dc2b3bba169180a4785bdd85
elbertribeiro <elbertprofissional@gmail.com>
2023-01-16 16:36:36 -0300

    refactory: migration java for kotlin

---
+++ src/main/kotlin/one/digitalinnovation/personapi/PersonapiApplication.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.personapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PersonapiApplication
+
+fun main(args: Array<String>) {
+    runApplication<PersonapiApplication>(*args)
+}
