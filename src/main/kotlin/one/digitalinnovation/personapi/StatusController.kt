ec33b5e69b71efdfb82e5c7a4d0789373005eb30
elbertribeiro <elbertprofissional@gmail.com>
2023-01-15 22:35:41 -0300

    build: add kotlin

---
+++ src/main/kotlin/one/digitalinnovation/personapi/StatusController.kt
@@ -0,0 +1,16 @@
+package one.digitalinnovation.personapi
+
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/v1/status")
+class StatusController {
+    @GetMapping
+    fun getStatus(): ResponseEntity<String> {
+        return ResponseEntity("Ok", HttpStatus.OK)
+    }
+}
